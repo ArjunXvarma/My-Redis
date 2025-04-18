2e1d336124810315fa6f2b9cec5f9063dd1a6f12
ArjunXvarma <arj15835@gmail.com>
2025-04-18 12:09:30 +0530

    Implemented non blocking server

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -1,10 +1,10 @@
-// C++ program to illustrate the client application in the
-// socket programming
 #include <cstring>
 #include <iostream>
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <unistd.h>
+
+using namespace std;
 
 int main() {
     int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
@@ -14,13 +14,23 @@ int main() {
     serverAddress.sin_port = htons(8080);
     serverAddress.sin_addr.s_addr = INADDR_ANY;
 
-    connect(clientSocket, (struct sockaddr*)&serverAddress,
-            sizeof(serverAddress));
+    connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
 
     const char* message = "PING";
-    send(clientSocket, message, strlen(message), 0);
+    send(clientSocket, message, strlen(message) + 1, 0);
+
+    char buffer[1024] = {0};
+    int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
+    if (bytesReceived > 0) {
+        cout << "Message from server: " << buffer << endl;
+        if (strcmp(buffer, "PONG") == 0)
+            cout << "Received PONG from server" << endl;
+        else
+            cout << "Did not receive PONG from server" << endl;
+    } else {
+        cout << "No response from server" << endl;
+    }
 
     close(clientSocket);
-
     return 0;
 }
--- src/server/server.cpp
+++ src/server/server.cpp
@@ -1,39 +1,121 @@
-#include <cstring>
 #include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include <fcntl.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
-#include <unistd.h>
+#include <sys/select.h>
+#include <unordered_map>
 
 #define PORT 8080
+#define BUFFER_SIZE 1024
 
 using namespace std;
 
-int main() {
+int setNonBlocking(int fd) {
+    int flags = fcntl(fd, F_GETFL, 0);
+    if (flags == -1) return -1;
+    return fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+}
+
+int setupServerSocket(int port) {
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-
-    sockaddr_in serverAddress;
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(PORT);
-    serverAddress.sin_addr.s_addr = INADDR_ANY;
-
-    bind(serverSocket, (struct sockaddr*) &serverAddress, sizeof(serverAddress));
-
-    listen(serverSocket, 5);
-    cout << "Listening on " << PORT << endl;
-
-    while (true) {
-        int clientSocket = accept(serverSocket, nullptr, nullptr);
-
-        char buffer[1024] = { 0 };
-        recv(clientSocket, buffer, sizeof(buffer), 0);
-        cout << "Message from client: " << buffer << endl;
-        if (buffer == "PING") 
-            send(clientSocket, "PONG", sizeof("PONG"), 0);
-        
-        else break;
+    if (serverSocket < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
     }
 
-    close(serverSocket);
+    int opt = 1;
+    setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
+    sockaddr_in serverAddress{};
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(port);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(serverSocket, (sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) {
+        perror("Bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(serverSocket, 5) < 0) {
+        perror("Listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (setNonBlocking(serverSocket) < 0) {
+        perror("Non-blocking failed");
+        exit(EXIT_FAILURE);
+    }
+
+    return serverSocket;
+}
+
+void handleClient(int clientSocket) {
+    char buffer[BUFFER_SIZE] = {0};
+    ssize_t bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
+    if (bytesReceived <= 0) {
+        close(clientSocket);
+        return;
+    }
+
+    cout << "Received from client: " << buffer << endl;
+
+    if (strcmp(buffer, "PING") == 0) {
+        const char* response = "PONG";
+        send(clientSocket, response, strlen(response), 0);
+    } else {
+        const char* response = "UNKNOWN COMMAND";
+        send(clientSocket, response, strlen(response), 0);
+    }
+}
+
+void runEventLoop(int serverSocket) {
+    fd_set masterSet, readSet;
+    FD_ZERO(&masterSet);
+    FD_SET(serverSocket, &masterSet);
+    int maxFd = serverSocket;
+
+    while (true) {
+        readSet = masterSet;
+        int activity = select(maxFd + 1, &readSet, nullptr, nullptr, nullptr);
+
+        if (activity < 0) {
+            perror("select() failed");
+            break;
+        }
+
+        for (int fd = 0; fd <= maxFd; ++fd) {
+            if (FD_ISSET(fd, &readSet)) {
+                if (fd == serverSocket) {
+                    // Accept new client
+                    int clientSocket = accept(serverSocket, nullptr, nullptr);
+                    if (clientSocket < 0) {
+                        perror("accept() failed");
+                        continue;
+                    }
+
+                    setNonBlocking(clientSocket);
+                    FD_SET(clientSocket, &masterSet);
+                    if (clientSocket > maxFd) maxFd = clientSocket;
+
+                    cout << "Accepted new connection (fd=" << clientSocket << ")" << endl;
+                } else {
+                    // Handle client message
+                    handleClient(fd);
+                    FD_CLR(fd, &masterSet);
+                }
+            }
+        }
+    }
+}
+
+int main() {
+    int serverSocket = setupServerSocket(PORT);
+    cout << "Server listening on port " << PORT << "..." << endl;
+
+    runEventLoop(serverSocket);
+
+    close(serverSocket);
     return 0;
 }
