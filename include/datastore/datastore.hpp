de36c35e8b0da56cfa84debd7dcb08f05afadcf3
ArjunXvarma <arj15835@gmail.com>
2025-05-27 08:28:12 +0530

    Updated command dispatcher and datastore

--- include/datastore/datastore.hpp
+++ include/datastore/datastore.hpp
@@ -4,21 +4,58 @@
 #include <string>
 #include <optional>
 #include <iostream>
+#include <mutex>
+#include <vector>
 
 #include "../persistence/persistenceManager.hpp"
+#include "Value.hpp"
 
 class DataStore {
-public:
-    static DataStore& getInstance();
-    void set(const std::string& key, const std::string& value);
-    void del(const std::string& key);
-    std::optional<std::string> get(const std::string& key) const;
-    void save();
-
 private:
-    std::unordered_map<std::string, std::string> store;
-
+    std::unordered_map<std::string, Value> store;
+    mutable std::mutex m;
+    
+public:
     DataStore();
     DataStore(const DataStore&) = delete;
+
+    static DataStore& getInstance();
+
+    // List commands
+    void lpush(const std::string key, const std::vector<std::string> &values);
+    void rpush(const std::string key, const std::vector<std::string> &values);
+    void lpop(const std::string key, const int n);
+    void rpop(const std::string key, const int n);
+    std::vector<std::string> lrange(const std::string& key, int start, int end);
+    size_t llen(const std::string& key);
+
+    // Set commands
+    int sadd(const std::string key, const std::vector<std::string> &values);
+    int srem(const std::string key, const std::vector<std::string> &values);
+    std::vector<std::string> smembers(const std::string key);
+    bool sismember(const std::string key, const std::string value);
+
+    // Hash Set commands
+    void hset(const std::string hash_key, const std::string key, const std::string value);
+    std::string hget(const std::string hash_key, const std::string key);
+    void hdel(const std::string hash_key, const std::string key);
+    std::vector<std::string> hgetall(const std::string hash_key);
+    bool hexists(const std::string hash_key, const std::string key);
+
+    void set(const std::string& key, const std::string &value);
+    bool del(const std::string& key);
+    std::optional<std::string> get(const std::string &key) const;
+    void flushAll();
+    std::vector<std::string> allKeys();
+    std::vector<std::optional<std::string>> mget(const std::vector<std::string> &keys);
+    void mset(const std::unordered_map<std::string, std::string>& kvs);
+    void scan();
+    void sinter();
+    void sort();
+    void sunion();
+    void zrange();
+    
+    void save();
+    
     DataStore& operator=(const DataStore&) = delete;
 };
--- src/command/commandDispatcher.cpp
+++ src/command/commandDispatcher.cpp
@@ -16,6 +16,38 @@ std::string CommandDispatcher::dispatch(const std::vector<std::string>& tokens) 
         if (cmd == "SET") return SetCommand().execute(args);
         if (cmd == "GET") return GetCommand().execute(args);
 
+        if (cmd == "DEL") return DelCommand().execute(args);
+        if (cmd == "FLUSHALL") return FlushAllCommand().execute(args);
+        if (cmd == "KEYS") return KeysCommand().execute(args);
+        if (cmd == "MGET") return MGetCommand().execute(args);
+        if (cmd == "MSET") return MSetCommand().execute(args);
+        if (cmd == "SCAN") return ScanCommand().execute(args);
+        if (cmd == "SINTER") return SInterCommand().execute(args);
+        if (cmd == "SORT") return SortCommand().execute(args);
+        if (cmd == "SUNION") return SUnionCommand().execute(args);
+        if (cmd == "ZRANGE") return ZRangeCommand().execute(args);
+
+        // Set commands
+        if (cmd == "SADD") return SAddCommand().execute(args);
+        if (cmd == "SISMEMBER") return SIsMemberCommand().execute(args);
+        if (cmd == "SMEMBERS") return SMembersCommand().execute(args);
+        if (cmd == "SREM") return SRemCommand().execute(args);
+
+        // List Commands
+        if (cmd == "LLEN") return LLenCommand().execute(args);
+        if (cmd == "LPOP") return LPopCommand().execute(args);
+        if (cmd == "LPUSH") return LPushCommand().execute(args);
+        if (cmd == "LRANGE") return LRangeCommand().execute(args);
+        if (cmd == "RPOP") return RPopCommand().execute(args);
+        if (cmd == "RPUSH") return RPushCommand().execute(args);
+
+        // Hash set commands
+        if (cmd == "HDEL") return HDelCommand().execute(args);
+        if (cmd == "HEXISTS") return HExistsCommand().execute(args);
+        if (cmd == "HGETALL") return HGetAllCommand().execute(args);
+        if (cmd == "HGET") return HGetCommand().execute(args);
+        if (cmd == "HSET") return HSetCommand().execute(args);
+
         return "-ERR unknown command '" + cmd + "'\r\n";
     });
 
